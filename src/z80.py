3a006b877872ea014edbad242cb7088c8ae499bd
Kenny Van de Maele <kenny@adimian.com>
2023-01-27 19:52:21 +0100

    format using black
    Signed-off-by: Kenny Van de Maele <kenny@adimian.com>

--- src/z80.py
+++ src/z80.py
@@ -1,31 +1,25 @@
 from memorymapper import MemoryMapper
 
+
 class Z80:
     def __init__(self):
         self.registers = {
-            'A': 0x00, 
-            'B': 0x00, 
-			'C': 0x00,
-            'D': 0x00, 
-			'E': 0x00,
-            'H': 0x00, 
-			'L': 0x00,
-			'F': 0x00,
-			'I': 0x00,
-			'R': 0x00,
-            'SP': 0x0000, 
-            'PC': 0x0000, 
-			'IX': 0x0000,
-			'IY': 0x0000
+            "A": 0x00,
+            "B": 0x00,
+            "C": 0x00,
+            "D": 0x00,
+            "E": 0x00,
+            "H": 0x00,
+            "L": 0x00,
+            "F": 0x00,
+            "I": 0x00,
+            "R": 0x00,
+            "SP": 0x0000,
+            "PC": 0x0000,
+            "IX": 0x0000,
+            "IY": 0x0000,
         }
-        self.flags = {
-			'C':   0,
-			'N':   0,
-			'P/V': 0,
-			'H':   0,
-			'Z':   0,
-			'S':   0
-        }
+        self.flags = {"C": 0, "N": 0, "P/V": 0, "H": 0, "Z": 0, "S": 0}
         self.memory_mapper = MemoryMapper()
         self.OPCODES = {
             0x00: (self.NOP, 0),
@@ -35,18 +29,17 @@ class Z80:
             0x04: (self.INC_B, 0),
             0x05: (self.DEC_B, 0),
             0x06: (self.LD_B_d8, 1),
-          # 0x07: (self.RLCA, 0),
-          # 0x08: (self.LD_a16_SP, 2),
-          # 0x09: (self.ADD_HL_BC, 0),
-          # 0x0A: (self.LD_A_BC, 0),
-          # 0x0B: (self.DEC_BC, 0),
-          # 0x0C: (self.INC_C, 0),
-          # 0x0D: (self.DEC_C, 0),
-          # 0x0E: (self.LD_C_d8, 1),
-          # 0x0F: (self.RRCA, 0)
+            # 0x07: (self.RLCA, 0),
+            # 0x08: (self.LD_a16_SP, 2),
+            # 0x09: (self.ADD_HL_BC, 0),
+            # 0x0A: (self.LD_A_BC, 0),
+            # 0x0B: (self.DEC_BC, 0),
+            # 0x0C: (self.INC_C, 0),
+            # 0x0D: (self.DEC_C, 0),
+            # 0x0E: (self.LD_C_d8, 1),
+            # 0x0F: (self.RRCA, 0)
             0x7E: (self.LD_A_HL, 0)
-
-			# TODO: Implement the rest
+            # TODO: Implement the rest
         }
 
     def decode(self, code):
@@ -56,13 +49,13 @@ class Z80:
             opcode = code[i]
             # Check if the opcode is a prefix
             if opcode == 0xCB:
-                opcode = code[i+1]
+                opcode = code[i + 1]
                 i += 1
             if opcode in self.OPCODES:
                 func, num_operands = self.OPCODES[opcode]
                 operands = []
                 for j in range(num_operands):
-                    operands.append(code[i+j+1])
+                    operands.append(code[i + j + 1])
                 func(*operands)
                 i += num_operands + 1
             else:
@@ -74,24 +67,24 @@ class Z80:
         pass
 
     def LD_BC_d16(self, operand1, operand2):
-        self.registers['B'] = operand1
-        self.registers['C'] = operand2
+        self.registers["B"] = operand1
+        self.registers["C"] = operand2
 
     def LD_BC_A(self):
-        self.memory[self.registers['BC']] = self.registers['A']
+        self.memory[self.registers["BC"]] = self.registers["A"]
 
     def INC_BC(self):
-        self.registers['BC'] += 1
+        self.registers["BC"] += 1
 
     def INC_B(self):
-        self.registers['B'] += 1
+        self.registers["B"] += 1
 
     def DEC_B(self):
-        self.registers['B'] -= 1
+        self.registers["B"] -= 1
 
     def LD_B_d8(self, operand):
-        self.registers['B'] = operand
+        self.registers["B"] = operand
 
     def LD_A_HL(self):
-        address = self.registers["HL"] # TODO: handle combined 2 byte registers
+        address = self.registers["HL"]  # TODO: handle combined 2 byte registers
         self.registers["A"] = self.memory_mapper.read_byte(address)
